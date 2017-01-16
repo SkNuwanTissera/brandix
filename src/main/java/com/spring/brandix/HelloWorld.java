f30bd788efb58f0245f232e7ecb3ebbc5350ea5e
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2017-01-16 12:01:27 +0530

    Add files via upload

---
+++ src/main/java/com/spring/brandix/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.spring.brandix;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+/**
+ * Created by Nuwan-PC on 10/01/2017.
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet{
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException,IOException {
+        res.getWriter().println("Hello World!!");
+    }
+}
