ae2535956e44f3747c3c136d4405ae9d257e725b
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2017-01-18 15:57:00 +0530

    Add files via upload

--- src/main/java/com/spring/brandix/HelloWorld.java
+++ src/main/java/com/spring/brandix/HelloWorld.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 /**
  * Created by Nuwan-PC on 10/01/2017.
  */
-@WebServlet("/hello")
+@WebServlet(name = "MyServlet",
+        urlPatterns = {"/hello"})
 public class HelloWorld extends HttpServlet{
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException,IOException {
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Nuwan-PC
+  Date: 18/01/2017
+  Time: 15:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>BQI</title>
+</head>
+<body>
+    <h2>HELLO WORLD!!!</h2>
+</body>
+</html>
